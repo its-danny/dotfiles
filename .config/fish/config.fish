3c9af14498c4c732833c4d0de8652cc2abf2f006
Danny Tatom <its.danny@hey.com>
2024-04-05 11:58:47 -0700

    add fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,11 +6,16 @@ starship init fish | source
 
 zoxide init fish | source
 
+# fzf - https://github.com/junegunn/fzf
+
+fzf --fish | source
+
 # Aliases
 
 alias dtf="/opt/homebrew/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias reload="source ~/.config/fish/config.fish"
 alias update="brew update; brew upgrade"
+alias clean="brew autoremove; brew cleanup"
 
 alias cat="bat" # https://github.com/sharkdp/bat
 alias find="fd" # https://github.com/sharkdp/fd
