e89f37d4d78704c8ce70cc38a92a01c27fe6556a
Danny Tatom <its.danny@hey.com>
2024-04-06 15:35:29 -0700

    add dadbod to neovim

--- .config/nvim/lua/plugins/additions.lua
+++ .config/nvim/lua/plugins/additions.lua
@@ -43,4 +43,21 @@ return {
       { "<leader>i", "<cmd>Portal jumplist forward<cr>", desc = "Portal Forward" },
     },
   },
+  -- Dev
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
