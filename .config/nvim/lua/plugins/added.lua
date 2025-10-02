7a6cee9c3e3fbb8eebab9452e6f3e24fab2f2ec0
Danny Tatom <danny@tatom.xyz>
2025-10-02 10:31:05 -0700

    add neorg

--- .config/nvim/lua/plugins/added.lua
+++ .config/nvim/lua/plugins/added.lua
@@ -3,4 +3,10 @@ return {
     "cbochs/grapple.nvim",
     event = { "BufReadPost", "BufNewFile" },
   },
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+  },
 }
